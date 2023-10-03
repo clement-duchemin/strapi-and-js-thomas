392a923cbf3c2d89306a10090055b42f1ab28dcd
webtoma <thomas.delalbre@gmail.com>
2023-10-03 16:42:09 +0200

    updating with single article page

--- my-website/frontend/index.js
+++ my-website/frontend/index.js
@@ -1,22 +1,52 @@
-fetch('http://localhost:1337/api/articles')
-.then(response => response.json())
-.then(data => buildArticles(data.data));
-
-
-function buildArticles(articles){
+function displayArticles(articles){
+    let divArticles = "";
     for(let i = 0; i < articles.length; i++){
-        const article = articles[i];
-        console.log(articles[i])
-        const articleElement = buildArticle(article);
-        document.querySelector('#articles').appendChild(articleElement);
+        let article = articles[i];
+        divArticles += `
+            <div class="col-md-4 text-center">
+                <h3>${article.attributes.title}</h3>
+                <p>${article.attributes.description}</p>
+                <a class="btn btn-primary" href="article.html?id=${article.id}">Read more</a>
+            </div>
+        `;
     }
+    document.getElementById('articles').innerHTML = divArticles;
+    console.log(divArticles);
 }
-function buildArticle(article){
-    const articleElement = document.createElement('div');
-    articleElement.classList.add('article');
-    articleElement.innerHTML = `
-        <h2>${article.attributes.title}</h2>
-        <p>${article.attributes.description}</p>
+function displaySingleArticle(article){
+    let divArticle = `
+        <div class="col-md-12 text-center">
+            <h3>${article.attributes.title}</h3>
+            <img src="http://localhost:1337${article.attributes.cover.data.attributes.url}" class="img-fluid">
+            <p>${article.attributes.description}</p>
+            <a class="btn btn-primary" href="index.html">Return to home</a>
+        </div>
     `;
-    return articleElement;
+    document.getElementById('single-article').innerHTML = divArticle;
+    console.log(divArticle);
+}
+let page = window.location.search
+console.log(page)
+// find html file in an url
+let index = "index.html"
+let article = "article.html"
+//check if page is index.html or article.html
+let url = window.location.href;
+let filename = url.split('/').pop();
+console.log(filename)
+filename = filename.split('.')[0];
+console.log(filename)
+
+if(filename == "index"){
+    //display all articles in index.html
+    fetch('http://localhost:1337/api/articles')
+    .then(response => response.json())
+    .then(data => displayArticles(data.data));
+}else if (filename == "article"){
+    //display Single article on article.html
+    let parametersURL = new URLSearchParams(window.location.search);
+    let id = parametersURL.get('id');
+    fetch(`http://localhost:1337/api/articles/${id}?populate=*`)
+    .then(response => response.json())
+    .then(data => displaySingleArticle(data.data));
 }
